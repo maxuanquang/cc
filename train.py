951d6b17114ba4c846eef74c5b9039cfe472e570
maxuanquang <maxuanquang@gmail.com>
2021-03-11 15:12:28 +0700

    Fix typo

--- train.py
+++ train.py
@@ -521,7 +521,7 @@ def train(train_loader, disp_net, pose_net, mask_net, flow_net, optimizer, epoch
             train_writer.add_scalar('flow_photometric_error', loss_4.item(), n_iter)
             train_writer.add_scalar('consensus_error', loss_5.item(), n_iter)
             train_writer.add_scalar('total_loss', loss.item(), n_iter)
-            with open(os.path.join(args.save_path,'n_iter.txt'),'a') as f:
+            with open(os.path.join(args.save_path,'n_iter.txt'),'w') as f:
                 f.write(str(n_iter))
 
 
