06004f268fbe1459661f7833195d8ac541686aae
maxuanquang <maxuanquang@gmail.com>
2021-05-13 09:38:36 +0700

    Change suspicious error in loss: (1-wssim)*wssim

--- loss_functions.py
+++ loss_functions.py
@@ -92,7 +92,7 @@ def photometric_reconstruction_loss(tgt_img, ref_imgs, intrinsics, intrinsics_in
         intrinsics_scaled = torch.cat((intrinsics[:, 0:2]/downscale, intrinsics[:, 2:]), dim=1)
         intrinsics_scaled_inv = torch.cat((intrinsics_inv[:, :, 0:2]*downscale, intrinsics_inv[:, :, 2:]), dim=2)
 
-        weight = 0.5
+        weight = 1.0
 
         for i, ref_img in enumerate(ref_imgs_scaled):
             current_pose = pose[:, i]
@@ -112,7 +112,7 @@ def photometric_reconstruction_loss(tgt_img, ref_imgs, intrinsics, intrinsics_in
                 diff = diff *(1-occ_masks[:,i:i+1]).expand_as(diff)
                 ssim_loss = ssim_loss*(1-occ_masks[:,i:i+1]).expand_as(ssim_loss)
 
-            reconstruction_loss +=  oob_normalization_const*((1- wssim)*robust_l1_per_pix(diff, q=qch) + weight*wssim*ssim_loss).min() + lambda_oob*robust_l1(1 - valid_pixels, q=qch)
+            reconstruction_loss +=  oob_normalization_const*((1- wssim)*robust_l1_per_pix(diff, q=qch) + weight*wssim*ssim_loss).mean() + lambda_oob*robust_l1(1 - valid_pixels, q=qch)
             assert((reconstruction_loss == reconstruction_loss).item() == 1)
             #weight /= 2.83
         return reconstruction_loss
